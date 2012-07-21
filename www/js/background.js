5182c267477b80fd13c3734a65298c1078b22b0e
Yuest Wang <yuestwang@gmail.com>
2012-07-21 22:58:05 +0800

    background app icons no duplication

--- www/js/background.js
+++ www/js/background.js
@@ -18,21 +18,25 @@ $(function() {
 	for (i = 0; i < showNum; i++) {
 		html += tIcon({a: 1, b: 1})
 	}
+	var hiddenImgs = []
 	var $icons = $(html)
 	$icons.find('.front img').each(function(i, el) {
 		a = j++ % iconNum + 1
 		$(el).attr('src', './img/appicons/icon_'+a+'.png')
-	}).end().find('.front img').each(function(i, el) {
-		a = j++ % iconNum + 1
-		$(el).attr('src', './img/appicons/icon_'+a+'.png')
 	}).end().appendTo($background)
+	for (i = 0; i < 20; i++) {
+		a = j++ % iconNum + 1
+		hiddenImgs.push(a)
+	}
 
 	$icons = $background.find('.icon')
 	function flip() {
 		var n = Math.floor(Math.random() * showNum)
 		var $icon = $icons.eq(n)
-		var k = Math.floor(Math.random() * iconNum) + 1
-		$icon.find($icon.hasClass('flip') ? '.front img' : '.back img').attr('src', './img/appicons/icon_'+k+'.png')
+		var $imgShowing = $icon.find($icon.hasClass('flip') ? '.back img' : '.front img')
+		var imgId = parseInt($imgShowing.attr('src').match(/icon_(\d{1,3})\.png/)[1], 10)
+		hiddenImgs.push(imgId)
+		$icon.find($icon.hasClass('flip') ? '.front img' : '.back img').attr('src', './img/appicons/icon_'+hiddenImgs.shift()+'.png')
 		$icon.toggleClass('flip')
 	}
 	function randomFlip() {
